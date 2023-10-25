c55d6cc318e016aa8cf6b9dd4dbee0017a60af52
eunjin Han <hanb5468@gmail.com>
2023-10-25 23:14:25 +0900

    change pageLayout

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const media = {
+  mobile: '360px',
+  laptop: '1100px',
+};
+
+const theme = {
+  mobile: `(max-width: ${media.mobile})`,
+  laptop: `(max-width: ${media.laptop})`,
+};
+
+export default theme;
--- src/templates/PageLayout.js
+++ src/templates/PageLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from 'styles/theme';
 
 const PageLayout = ({ children }) => {
   return (
@@ -10,7 +11,7 @@ const PageLayout = ({ children }) => {
 };
 
 const MobileWrap = styled.div`
-  width: 360px;
+  width: 100%;
   height: 100vh;
   margin: 0 auto;
   overflow-x: hidden;
@@ -18,6 +19,10 @@ const MobileWrap = styled.div`
   background-color: white;
   position: relative;
   padding: 0 8px;
+
+  @media ${theme.mobile} {
+    width: 360px;
+  }
 `;
 
 export default PageLayout;
